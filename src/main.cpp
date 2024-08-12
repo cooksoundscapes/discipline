b79d451599d1b1f739e481762bb89d515df30d4b
cooksoundscapes <paulobmedina@gmail.com>
2024-08-12 01:32:54 -0300

    unignoring lib

--- src/main.cpp
+++ src/main.cpp
@@ -1,43 +1,19 @@
-#include "raylib.h"
-#define RAYGUI_IMPLEMENTATION
-#include "raygui.h"
-
 #include <lua.hpp>
 #include <lo/lo.h>
-#include <iostream>
-#include <unordered_map>
-#include <functional>
+
 #include <string>
 #include <mutex>
 
-#define SCREEN_W 640
-#define SCREEN_H 480
-#define TARGET_FPS 60
-#define OSC_PORT "9000"
-#define HOME "home"
-
-/**
- * Inline helper functions
- */
-inline Color hex(const char* h) {
-    unsigned int r, g, b;
-    std::sscanf(h, "#%02x%02x%02x", &r, &g, &b);
-    return Color{
-        static_cast<unsigned char>(r),
-        static_cast<unsigned char>(g),
-        static_cast<unsigned char>(b),
-        255
-    };
-}
+#include "main.h"
 
 /**
  * Setup multiple screen mechanism
  */
-std::unordered_map<std::string, std::function<void()>> screens = {
-    {HOME, [](){
+std::unordered_map<std::string, screen> screens = {
+    {HOME, {[](){
         ClearBackground(hex("#2e2e2e"));
         DrawText("HIII BITCHES", SCREEN_W/3, SCREEN_W/3, 20, hex("#fafafa"));
-    }}
+    }, {}}}
 };
 std::mutex mtx;
 std::string current_screen{HOME};
@@ -58,6 +34,14 @@ int navigate_handler(const char* p, const char* t, lo_arg** argv, int argc, lo_m
     return 0;
 }
 
+int param_handler(const char* p, const char* t, lo_arg** argv, int argc, lo_message data, void* userData) {
+    std::lock_guard<std::mutex> lock(mtx);
+    auto key = &argv[0]->s;
+    auto value = &argv[1]->f;
+    screens[current_screen].set(key, *value);
+    return 0;
+}
+
 /**
  * Begin main function
  */
@@ -69,9 +53,11 @@ int main() {
     [](int num, const char *msg, const char *path){ // error handler
         std::cout << "OSC server error " << num << " in path " << path << ": " << msg << '\n';
     });
-    lo_server_thread_add_method(server, "/navigate", "s", navigate_handler, &current_screen);
+    lo_server_thread_add_method(server, "/navigate", "s", navigate_handler, nullptr);
+    lo_server_thread_add_method(server, "/param", "sf", param_handler, nullptr);
     lo_server_thread_start(server);
     std::cout << "Listening to OSC messages at port " << OSC_PORT << ";\n";
+
     /**
      * Lua setup
      */
@@ -81,7 +67,7 @@ int main() {
     /**
      * Raylib setup
      */
-    InitWindow(SCREEN_W, SCREEN_H, "testing");
+    InitWindow(SCREEN_W, SCREEN_H, "Discipline");
     SetTargetFPS(TARGET_FPS);
 
     while (!WindowShouldClose()) {
---
+++ src/main.h
@@ -0,0 +1,49 @@
+#pragma once
+#include "raylib.h"
+#define RAYGUI_IMPLEMENTATION
+#include "raygui.h"
+
+#include <iostream>
+#include <unordered_map>
+#include <functional>
+
+#define SCREEN_W 640
+#define SCREEN_H 480
+#define TARGET_FPS 60
+#define OSC_PORT "9000"
+#define HOME "home"
+
+/**
+ * Inline helper functions
+ */
+inline Color hex(const char* h) {
+    unsigned int r, g, b;
+    std::sscanf(h, "#%02x%02x%02x", &r, &g, &b);
+    return Color{
+        static_cast<unsigned char>(r),
+        static_cast<unsigned char>(g),
+        static_cast<unsigned char>(b),
+        255
+    };
+}
+
+struct screen {
+    std::function<void()> draw;
+
+    std::unordered_map<const char*, float> params;
+    void operator()() {
+        draw();
+    }
+
+    void set(const char* k, float v) {
+        if (params.find(k) != params.end()) {
+            params[k] = v;
+        } else {
+            params.insert({k, v});
+        }
+    }
+
+    float get(const char* k) {
+        return params[k];
+    }
+};
